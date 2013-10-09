76e8b6302fe25f5868fe892ad75c2df79b598b11
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2013-10-09 18:00:16 +0100

    Validate presence of all fields on Organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,8 @@
 class Organisation < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  validates :api_id, presence: true
+  validates :web_url, presence: true
+  validates :name, presence: true
+  validates :organisation_type, presence: true
 end
